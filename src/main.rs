37f566ecb627e43b059cb13c5e15b6940047d0aa
Dale Wijnand <dale.wijnand@gmail.com>
2018-05-18 18:28:22 +0200

    mkdir project

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 //! Create a new sbt build in an existing directory
 //! Author: Dale Wijnand <dale.wijnand@gmail.com>
 
-fn main() {
-    // mkdir project
+#[allow(unused_imports)]
+use std::io::{self, prelude::*};
+use std::fs;
+
+fn main() -> io::Result<()> {
+    fs::create_dir("project")?;
+    Ok(())
     // sbt-set-version 1.1.5
     // cat > build.sbt << EOL
     // val t = project in file(".")
@@ -12,5 +17,4 @@ fn main() {
     // scalaVersion in ThisBuild := "2.12.4"
     // EOL
     // touch project/plugins.sbt
-    println!("Hello, world!");
 }
