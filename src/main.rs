71c69210fd7f3b8687e649a265dd4a718deda5a7
Dale Wijnand <dale.wijnand@gmail.com>
2018-05-18 18:20:04 +0200

    Add original bash code

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,15 @@
 //! Author: Dale Wijnand <dale.wijnand@gmail.com>
 
 fn main() {
+    // mkdir project
+    // sbt-set-version 1.1.5
+    // cat > build.sbt << EOL
+    // val t = project in file(".")
+    //
+    // organization in ThisBuild := "com.dwijnand"
+    //      version in ThisBuild := "0.1.0-SNAPSHOT"
+    // scalaVersion in ThisBuild := "2.12.4"
+    // EOL
+    // touch project/plugins.sbt
     println!("Hello, world!");
 }
