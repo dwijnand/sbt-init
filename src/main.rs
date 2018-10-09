c6c85edc44563225de82b0e1788dff463add3ece
Dale Wijnand <dale.wijnand@gmail.com>
2018-10-09 09:15:18 +0100

    sbt 1.2.4/scala 2.12.7

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ use std::io;
 
 fn main() -> io::Result<()> {
     fs::create_dir_all("project")?;
-    fs::write("project/build.properties", "sbt.version=1.2.1")?;
+    fs::write("project/build.properties", "sbt.version=1.2.4")?;
     fs::write("project/plugins.sbt", "")?;
     fs::write("build.sbt", r#"
 val t = project in file(".")
 
 organization in ThisBuild := "com.dwijnand"
      version in ThisBuild := "0.1.0-SNAPSHOT"
-scalaVersion in ThisBuild := "2.12.6"
+scalaVersion in ThisBuild := "2.12.7"
 "#.trim_left())?;
     fs::create_dir_all("src/main/scala/t")?;
     fs::write("src/main/scala/t/Main.scala", r#"
