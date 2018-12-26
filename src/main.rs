1876c0d1d98412a559bf0e5b2c632d80d3239d68
Dale Wijnand <dale.wijnand@gmail.com>
2018-12-26 13:42:01 +0000

    scala 2.12.8

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ val t = project in file(".")
 
 organization in ThisBuild := "com.dwijnand"
      version in ThisBuild := "0.1.0-SNAPSHOT"
-scalaVersion in ThisBuild := "2.12.7"
+scalaVersion in ThisBuild := "2.12.8"
 "#.trim_left())?;
     fs::create_dir_all("src/main/scala/t")?;
     fs::write("src/main/scala/t/Main.scala", r#"
