bbd29733e96e2146f380ee45c2bd854e72d40b3d
Dale Wijnand <dale.wijnand@gmail.com>
2020-01-31 19:46:48 +0000

    Try out better-files instead

--- build.sbt
+++ build.sbt
@@ -5,5 +5,7 @@ organization in ThisBuild := "com.dwijnand"
      version in ThisBuild := "0.1.0-SNAPSHOT"
 scalaVersion in ThisBuild := "2.13.1"
 
+libraryDependencies += "com.github.pathikrit" %% "better-files" % "3.8.0"
+
 enablePlugins(GraalVMNativeImagePlugin)
 graalVMNativeImageOptions := List("--no-fallback")
--- src/main/scala/sbtinit/Main.scala
+++ src/main/scala/sbtinit/Main.scala
@@ -1,23 +1,23 @@
 package sbtinit
 
-import java.nio.file.{ Files, Paths }
+import better.files._
 
 object Main {
   val sbtVersion = "1.3.7"
 
   def main(args: Array[String]): Unit = {
-    Files.createDirectory(Paths.get("project"))
-    Files.writeString(Files.createFile(Paths.get("project/build.properties")), s"sbt.version=$sbtVersion\n")
-    Files.writeString(Files.createFile(Paths.get("project/plugins.sbt")), "")
-    Files.writeString(Files.createFile(Paths.get("build.sbt")), s"""
+    file"project".createDirectory()
+    file"project/build.properties".createFile().write(s"sbt.version=$sbtVersion\n")
+    file"project/plugins.sbt".createFile()
+    file"build.sbt".createFile().write(s"""
       |val t = project in file(".")
       |
       |organization in ThisBuild := "com.dwijnand"
       |     version in ThisBuild := "0.1.0-SNAPSHOT"
       |scalaVersion in ThisBuild := "2.13.1"
       |""".stripMargin.stripLeading())
-    Files.createDirectories(Paths.get("src/main/scala/t"))
-    Files.writeString(Files.createFile(Paths.get("src/main/scala/t/Main.scala")), s"""
+    file"src/main/scala/t".createDirectories()
+    file"src/main/scala/t/Main.scala".createFile().write(s"""
       |package t
       |
       |object Main {
